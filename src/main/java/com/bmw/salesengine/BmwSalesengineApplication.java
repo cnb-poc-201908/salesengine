d98da729824abbb083ac9c1752264d9c969c792f
yuanbing1113 <yuanbing1113@gmail.com>
2019-08-16 12:47:37 +0800

    first commit

---
+++ src/main/java/com/bmw/salesengine/BmwSalesengineApplication.java
@@ -0,0 +1,13 @@
+package com.bmw.salesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwSalesengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmwSalesengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmw/salesengine/BmwSalesengineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bmw.salesengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BmwSalesengineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
